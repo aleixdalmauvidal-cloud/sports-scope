e3badbed5ed99de9909ba5c8f2854a102055c5e0
Pau Busquets Mateu <paubusquets@MacBook-Air-de-Pau.local>
2026-04-13 15:26:24 +0200

    Allow api-sports.io images in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.api-sports.io',
+        pathname: '/football/players/**',
+      },
+    ],
   },
 }
 
